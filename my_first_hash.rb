af9a46a8c356ae21bdc551d331087ccab5030de2
Ruel Tingling <rvtingling@gmail.com>
2019-10-24 17:10:47 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,3 +1,4 @@
+# this lab not submitting to when i enter learn submit 
 def my_hash 
   {
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
